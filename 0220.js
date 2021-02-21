2e2d5ca0e218b0bfec6857bdbb01e92b92861427
jacksonzeng <864221141@qq.com>
2021-02-21 22:57:20 +0800

    feat: new

--- 0220.js
+++ 0220.js
@@ -1,7 +1,7 @@
 /*
  * @Author: jacksonzeng
  * @Date: 2021-02-20 17:19:29
- * @LastEditTime: 2021-02-20 22:00:26
+ * @LastEditTime: 2021-02-21 14:47:04
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /algorithm-collation/0220.js
@@ -167,3 +167,131 @@ function clearHashObject(hashObj, index) {
 }
 
 
+function findMedianSortedArrays(nums1, nums2) {
+    let m = nums1.length;
+    let n = nums2.length;
+    if ((m + n) == 1) return m == 1 ? nums1[0] : nums2[0];
+    let left = 0;
+    let right = m;
+    let temp;
+    if (m > n) {
+        right = n;
+        temp = nums1;//注意这种写法
+        nums1 = nums2;
+        nums2 = temp;
+    }
+    let total = (m + n + 1) / 2;
+    while (left < right) {
+        let i = left + (right - left) / 2;
+        let j = total - i;
+        if (nums1[i] > nums2[j - 1]) {
+            right = i;
+        }
+        else {
+            left = i + 1;
+        }
+    }
+    let i = left;
+    let j = total - i;
+    let nums1LValue = i - 1 >= 0 ? nums1[i - 1] : -Infinity;
+    let nums2LValue = j - 1 >= 0 ? nums2[j - 1] : -Infinity;
+    let nums1RValue = i >= nums1.length ? Infinity : nums1[i];
+    let nums2Rvalue = j >= nums2.length ? Infinity : nums2[j];
+    return (m + n) % 2 == 0 ? parseFloat(Math.min(nums1RValue, nums2Rvalue) + Math.max(nums1LValue, nums2LValue)) / 2 : Math.max(nums1LValue, nums2LValue);
+}
+
+function countingMiddle(nums1, nums2) {
+
+    let m = nums1.length;
+    let n = nums2.length;
+    if ((m + n) == 1) return m == 1 ? nums1[0] : nums2[0];
+    let left = 0;
+    let right = m;
+    let temp;
+    if (m > n) {
+        right = n;
+        temp = nums1;//注意这种写法
+        nums1 = nums2;
+        nums2 = temp;
+    }
+    let total = Math.floor((m + n + 1) / 2);
+    let i, j;
+    while (left < right) {
+        i = left + Math.floor((right - left) / 2);
+        j = total - i;
+        if (nums1[i] > nums2[j - 1]) {
+            //搜索区间[left,i]，要注意区间只有两个数时是否会无限循环
+            right = i;
+        }
+        else {
+            //搜索区间[i+1,right]，要注意区间只有2个数是否会无限循环
+            left = i + 1;
+        }
+    }
+    i = left;
+    j = total - i;
+    let lvalue, rvalue;
+    if (i - 1 < 0) {
+        lvalue = nums2[j - 1];
+    }
+    else if (j - 1 < 0) {
+        lvalue = nums1[i - 1];
+    }
+    else lvalue = nums1[i - 1] > nums2[j - 1] ? nums1[i - 1] : nums2[j - 1];
+    if (i >= nums1.length) {
+        rvalue = nums2[j];
+    }
+    else if (j >= nums2.length) {
+        rvalue = nums1[i];
+    }
+    else rvalue = nums1[i] < nums2[j] ? nums1[i] : nums2[j];
+
+    return (m + n) % 2 == 0 ? (parseFloat(lvalue + rvalue) / 2) : lvalue;
+}
+
+let res = countingMiddle([1, 2, 3, 4], [5, 6, 7, 8, 9, 10, 11]);
+console.log(res);
+
+
+
+
+
+
+/**
+ * @url https://leetcode-cn.com/problems/median-of-two-sorted-arrays/submissions/
+ * @description 注意Math.floor提前处理k1、k2
+ *              递归、分治算法
+ * @param {number[]} nums1 
+ * @param {number[]} nums2 
+ * @return {number}
+ */
+var findMedianSortedArrays = function findMedianSortedArrays(nums1, nums2) {
+    let len1 = nums1.length,
+        len2 = nums2.length;
+    let k1 = Math.floor((len1 + len2 + 1) / 2);
+    let k2 = Math.floor((len1 + len2 + 2) / 2);
+    return (getKthPlaceVal(nums1, 0, nums2, 0, k1) + getKthPlaceVal(nums1, 0, nums2, 0, k2)) / 2
+}
+
+function getKthPlaceVal(nums1, start1, nums2, start2, k) {
+    let len1 = nums1.length - start1;
+    let len2 = nums2.length - start2;
+    if (len1 <= 0) {
+        return nums2[start2 + k - 1]
+    }
+    if (len2 <= 0) {
+        return nums1[start1 + k - 1]
+    }
+    if (k === 1) {
+        return Math.min(nums1[start1], nums2[start2])
+    }
+    let k_half = Math.floor(k / 2);
+    // 这里要防止数组越界
+    let comparePlace1 = start1 + Math.min(k_half, len1) - 1;
+    let comparePlace2 = start2 + Math.min(k_half, len2) - 1;
+    if (nums1[comparePlace1] > nums2[comparePlace2]) {
+        return getKthPlaceVal(nums1, start1, nums2, comparePlace2 + 1, k - (comparePlace2 - start2 + 1))
+    } else {
+        return getKthPlaceVal(nums1, comparePlace1 + 1, nums2, start2, k - (comparePlace1 - start1 + 1))
+    }
+}
---
+++ 0221.js
@@ -0,0 +1,248 @@
+/*
+ * @Author: your name
+ * @Date: 2021-02-21 14:50:01
+ * @LastEditTime: 2021-02-21 22:50:01
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /algorithm-collation/0221.js
+ */
+
+
+/**
+ * @url https://leetcode-cn.com/problems/longest-palindromic-substring/submissions/
+ * @description 中心扩散 判断
+ * @param {string} s 
+ */
+var longestPalindrome = function (s) {
+    if (!s) return '';
+    const stepGap = 0.5;
+    const strLength = s.length;
+    let centerIndex = 0;
+    let maxCircleStr = s[0];
+    while (centerIndex <= strLength) {
+        let res = getCenterString(s, centerIndex);
+        if (res.length > maxCircleStr.length) {
+            maxCircleStr = res;
+        }
+        centerIndex += stepGap;
+    }
+    return maxCircleStr;
+}
+function getCenterString(originStr, centerIndex) {
+
+    let leftIndex = Math.floor(centerIndex);
+    let rightIndex = Math.ceil(centerIndex);
+    while (originStr[leftIndex] === originStr[rightIndex] && originStr[leftIndex] !== undefined) {
+        leftIndex--;
+        rightIndex++;
+    }
+    return originStr.slice(leftIndex + 1, rightIndex)
+}
+
+const resstring = longestPalindrome('bbbbaasaabc');
+console.log(resstring);
+
+
+/**
+ * @description 动态规划解法
+ *              时空复杂度更高
+ * @param {string} s 
+ */
+var longestPalindrome = function (s) {
+    let n = s.length;
+    let ans = []
+
+    let dq = [];
+    for (let index = 0; index < s.length; index++) {
+        dq.push([])
+    }
+    // boolean[][] dq = new boolean[n][n];
+    let max = 1;
+    for (let j = 0; j < n; j++) {
+        for (let i = 0; i < j; i++) {
+            dq[i][j] = (s.charAt(i) == s.charAt(j)) && (j - i < 3 || dq[i + 1][j - 1]);
+            if (dq[i][j] && max < j - i + 1) {
+                max = j - i + 1;
+                ans[0] = i;
+                ans[1] = j;
+            }
+        }
+    }
+    return s.substring(ans[0], ans[1] + 1);
+}
+
+
+
+
+/**
+ * @url https://leetcode-cn.com/problems/regular-expression-matching/comments/
+ * @description 实现正则表达式匹配 
+ *              动态规划  借助递归来回溯
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function (s, p) {
+    if (!p) return !s;
+
+    const isFirstMatch = s && (p[0] === s[0] || p[0] === '.');
+
+    if (p.length >= 2 && p[1] === '*') {
+        return isMatch(s, p.slice(2)) || (isFirstMatch && isMatch(s.slice(1), p));
+    } else {
+        return isFirstMatch && isMatch(s.slice(1), p.slice(1));
+    }
+};
+
+/**
+ * @description 尝试通过构造状态机的方式来处理
+ * @param {string} s 
+ * @param {*} p 
+ */
+var isMatch = function (s, p) {
+    const stateMachine = makeStateMachine(p);
+    return stateMachine.checkStringMatchOrNot(s)
+    // stateMachine.turnStateChange(6, "a")
+}
+
+function makeStateMachine(p) {
+    let characterArr = [];
+    for (let index = 0; index < p.length; index++) {
+        if (p[index] && p[index + 1] === '*') {
+            characterArr.push(p[index] + p[index + 1]);
+        } else if (p[index] !== "*") {
+            characterArr.push(p[index])
+        }
+    }
+    console.log(characterArr);
+    return new StateMachine({ character: characterArr });
+}
+
+class StateMachine {
+    constructor({ character }) {
+        /**
+         * schema like
+         * interface Rule{
+         *      state: number,
+         *      input: string,
+         *      output: number,
+         *      loop: boolean,
+         * }
+         */
+        this.stateChangeRules = [];
+        this.originCharacter = character;
+        this.finalStateCount = 0;
+        this.availableShiftStateArr = [];
+        this.init();
+    }
+    init() {
+        // 初始状态下的state为0
+        let countingState = 0;
+        this.originCharacter.forEach(characterStr => {
+            if (characterStr.length === 1) {
+                this.stateChangeRules.push({
+                    state: countingState,
+                    input: this.judgeSpecialCharacter(characterStr),
+                    output: countingState + 1,
+                    loop: false,
+                })
+                countingState++;
+            } else if (characterStr[1] === "*") {
+                if (this.stateChangeRules[this.stateChangeRules.length - 1] &&
+                    this.stateChangeRules[this.stateChangeRules.length - 1].loop === true) {
+                    countingState++
+                    this.stateChangeRules.push({
+                        state: countingState,
+                        input: this.judgeSpecialCharacter(characterStr.slice(0, 1)),
+                        output: countingState,
+                        loop: true,
+                    })
+                    // S1 可以自由切换到S2  不需要任何输入
+                    this.addNewShiftItem({
+                        input: countingState - 1,
+                        output: countingState,
+                    })
+                    // this.availableShiftStateArr.push({
+                    //     input: countingState - 1,
+                    //     output: countingState,
+                    // })
+                } else {
+                    this.stateChangeRules.push({
+                        state: countingState,
+                        input: this.judgeSpecialCharacter(characterStr.slice(0, 1)),
+                        output: countingState,
+                        loop: true,
+                    })
+                }
+                this.stateChangeRules[this.stateChangeRules.length - 1]
+            }
+        });
+        this.finalStateCount = countingState;
+        console.log(this.stateChangeRules)
+        console.log('this.availableShiftStateArr');
+        console.log(this.availableShiftStateArr);
+    }
+    checkStringMatchOrNot(str) {
+        let stateOutput = [0];
+        let newStateOutput = [];
+        for (let index = 0; index < str.length; index++) {
+            stateOutput = this.expandAvailableState(stateOutput);
+            stateOutput.forEach(state => {
+                newStateOutput = newStateOutput.concat(this.turnStateChange(state, str[index]));
+            })
+            stateOutput = newStateOutput;
+            newStateOutput = [];
+        }
+        // 最后做一次兜底保障 由于这句话只在循环体里执行 所以这里要补充一句
+        stateOutput = this.expandAvailableState(stateOutput);
+        console.log(stateOutput);
+        console.log(`checkStringMatchOrNot result: ${stateOutput.includes(this.finalStateCount)}`);
+        return stateOutput.includes(this.finalStateCount)
+    }
+    expandAvailableState(states) {
+        const expandStates = []
+        states.forEach(state => {
+            this.availableShiftStateArr.forEach(shift => {
+                if (shift.input === state) {
+                    expandStates.push(shift.output)
+                }
+            })
+        })
+        states = Array.from(new Set([...states, ...expandStates]));
+        console.log(`expandStates: ${expandStates}`);
+        console.log(states);
+        return states;
+    }
+    turnStateChange(state, input) {
+        const stateOutput = []
+        this.stateChangeRules.forEach(rule => {
+            if (rule.state === state && (rule.input === input || rule.input === "any")) {
+                stateOutput.push(rule.output)
+            }
+        })
+        console.log(`stateChange: ${state} , ${input} , ${JSON.stringify(stateOutput)}`);
+        return stateOutput;
+    }
+    judgeSpecialCharacter(character) {
+        return character === '.' ? 'any' : character
+    }
+    addNewShiftItem(shiftItem) {
+        // S1 -> S2 , S2 -> S3 意味着 S1 -> S3
+        const extraItem = []
+        this.availableShiftStateArr.forEach(item => {
+            if (item.output === shiftItem.input) {
+                extraItem.push({
+                    input: item.input,
+                    output: shiftItem.output,
+                })
+            }
+        })
+        this.availableShiftStateArr = [...this.availableShiftStateArr, ...extraItem, shiftItem]
+    }
+
+}
+
+isMatch('', "c*c*")
+
+
+
